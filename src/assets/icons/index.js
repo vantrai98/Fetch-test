075a45a4f0f34c8642959e972a95de768cbe3080
Mai Van Trai <maivantrai@gmail.com>
2023-01-07 16:41:19 +0700

    Add icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+export { default as chevronLeftIcon } from "./chevron-left.svg";
+export { default as chevronRightBlueIcon } from "./chevron-right-blue.svg";
+export { default as homeIcon } from "./home.svg";
+export { default as bellIcon } from "./bell.svg";
+export { default as walletIcon } from "./wallet.svg";
+export { default as personIcon } from "./person.svg";
